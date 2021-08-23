49e31fe0687825a51fdb977355b70e278757146b
Madsanka <mwattale@gmail.com>
2021-08-23 07:34:33 +0530

    Inserted created notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
 
 function App() {
     return (
         <div>
             <Header />
+            {notes.map(notecard =>
+            <Note  
+                id={notecard.id}
+                title={notecard.title}
+                content={notecard.content}
+            />
+            )}
             <Footer />
-            <Note />
         </div>
     )
 }
